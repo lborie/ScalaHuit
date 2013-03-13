88ca657bb7bdf2a5cba96cf23c924c5e25615219
Ludovic Borie <l.borie@kanithael.net>
2013-03-14 00:42:38 +0100

    Premier commit, mise en place des exercices de base

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 ScalaHuit
 =========
 
-Hand's On Scala, initiation aux bases, et plus si affinités
+Hand's On Scala, initiation aux bases, et plus si affinités.
+Ce projet est plus qu'inspiré du Hand's on Scala https://github.com/flatMapDuke/handson-scala
+(Des exercices en moins, d'autres différents)
+
+Pour générer les configurations des projets Eclipse ou IntelliJ, faire : 
+- Eclipse : sbt eclipe
+- IntelliJ : sbt gen-idea
---
+++ src/test/scala/I_Syntaxe/e1_Immutabilite.scala
@@ -0,0 +1,24 @@
+package I_Syntaxe
+
+/**
+*  Immutabilité ?
+*/ 
+class e1_Immutabilite extends HandsOnSuiteP1 {
+
+  test("Les vars peuvent être réaffectées") {
+    var a = 5
+    a should be(__)
+
+    a = 7
+    a should be(__)
+  }
+
+  test("Par contre les vals sont immuables (équivalent de final Java), elles ne peuvent pas être réaffectées") {
+    val a = 5
+    a should be(__)
+
+    // Qu'est-ce qui se passe lorsque l'on ajoute ces lignes ?
+    // a = 7
+    // a should be (7)
+  }
+}
