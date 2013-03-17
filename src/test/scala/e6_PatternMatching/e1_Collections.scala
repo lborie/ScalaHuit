28f43033ad28f2cd5b30b3feebfd8a35600e1447
Ludovic Borie <borie.l@sfeir.com>
2013-03-17 15:38:57 +0100

    Ajout de la partie Pattern Matching (cherry picked from commit 8dbcf206ca318383c8596a98266dbb43aeb46902)

---
+++ src/test/scala/e6_PatternMatching/e1_Collections.scala
@@ -0,0 +1,23 @@
+package e6_PatternMatching
+
+/**
+ * Un pattern matching est capable de reconnaitre la
+ * structure d'une Collection
+ */
+object e1_Collections {
+  def taille(list: List[Any]) : Int = {
+    list match {
+      case Nil => ???
+      case head :: tail => ???
+    }
+  }
+}
+
+class e1_Collections extends HandsOnSuiteP6 {
+  import e1_Collections.taille
+
+  test("Sur les collections ?") {
+    val myList = List("toto", "tata", 2, 3, 42)
+    taille(myList) should be(5)
+  }
+}
